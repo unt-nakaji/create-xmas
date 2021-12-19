e82e38791dd21b06c045220bc13afe2dca2f055a
nakaji <nakaji@un-t.com>
2021-12-20 08:32:45 +0900

    path変更

--- gulpfile.js/config.js
+++
@@ -1,9 +0,0 @@
-const mode = require('gulp-mode')();
-const isProduction = mode.production();
-const destDir = isProduction ? 'build' : 'dest';
-
-module.exports = {
-  END_OF_LINE: 'LF',
-  ENCODING: 'UTF8',
-  destDir,
-};
--- gulpfile.js/ssi.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const { destDir } = require('./config');
-
-const ssi = () => {
-  return gulp.src('./static/_ssi/*.inc', { since: gulp.lastRun(ssi) })
-    .pipe(gulp.dest(`${destDir}/_ssi`));
-};
-
-exports.ssi = ssi;
